09abb46ca5d3e7c66dcb1dd38e257e753fd24367
Achmad Reza Pahlevi <153707026+repal3@users.noreply.github.com>
2023-12-17 21:00:06 +0800

    Upload Percabangan 2

---
+++ Achmad Reza Pahlevi-231031097-Percabangan2.py
@@ -0,0 +1,7 @@
+#TUGAS PERCABANGAN 2
+
+inp = input('Masukkan suatu input: ')
+if len(inp) != 3:
+ print('Panjang string ', len(inp),'tidak sama dengan 3')
+else:
+ print('Panjang input sesuai yang diinginkan')
