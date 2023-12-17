d6c8fdf8cccc2345caab4b278ec3164a8fa556e7
Achmad Reza Pahlevi <153707026+repal3@users.noreply.github.com>
2023-12-17 20:54:51 +0800

    Upload Praktikum 5

---
+++ praktikum-d5.py
@@ -0,0 +1,56 @@
+import os
+os.system('cls')
+
+a = True
+i = 0
+limit = 3
+
+while a :
+    i +=1                           #i = i+1
+    if i <= limit:
+        print('Selamat Bergabung')
+    else:
+        a = False
+
+print()
+
+a = True
+i = 0
+limit = 3
+
+while a :
+    i +=1                           #i = i+1
+    if i <= limit:
+        print('Selamat Bergabung',i)
+    else:
+        a = False
+
+
+print()
+
+a = True
+i = 0
+limit = 3
+
+while a :
+    i +=1                           #i = i+1
+    if i <= limit:
+        print('Selamat Bergabung',4-i)
+    else:
+        a = False
+
+
+print()
+
+a = True
+i = 0
+limit = 3
+
+while a :
+    i +=1                           #i = i+1
+    if i <= limit:
+        print('Selamat Bergabung',i)
+    else:
+        print('Program berhenti di sini')
+        a = False
+    
